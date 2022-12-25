8ac3b628826c8251d99312389b39511f33c7cf94
amrlag <a.laghmich.dev@gmail.com>
2022-12-25 12:55:02 +0100

    Added our first SQL file

---
+++ reqSql_RPG.sql
@@ -0,0 +1,97 @@
+SELECT SUM(baseForce) AS "Points de force du jeu", SUM(baseAgi) AS "Points d'agilité du jeu", 
+SUM(baseIntel) AS "Points d'intelligence du jeu" 
+FROM classe;
+
+
+SELECT MIN(levelMin), MAX(levelMin) 
+FROM arme
+
+
+SELECT nom, baseForce+baseAgi+baseIntel AS "Points de caractéristiques"
+from classe
+
+SELECT nom, concat("F: ", baseForce, " - ", "A: ", baseAgi, " - ", "I: ", baseIntel) AS "Classe"
+FROM classe;
+
+/*ici, je concatène et fais deux substrings*/
+SELECT CONCAT(SUBSTRING(classe.nom,1,5)," - ", SUBSTRING(classe.description,1,20)) AS
+"CLASSE: "
+	
+FROM classe
+
+SELECT
+	*
+FROM
+	arme
+	WHERE
+    	degat<25
+		
+SELECT
+	nom, surnom
+FROM
+	personnage
+	WHERE
+    	level=10
+		
+SELECT
+	*
+FROM
+	typearme
+	WHERE
+    	estDistance=1
+		
+SELECT
+	*
+FROM
+	personnage
+WHERE
+	idPersonnage <=3 AND
+    level =10
+	
+SELECT
+	*
+FROM
+	arme
+WHERE
+	(idTypeArme=1 OR
+    idTypeArme=2) AND
+    (degat>=25 AND
+     degat<=40)
+	 
+SELECT
+	*
+FROM
+	arme
+WHERE
+	(idTypeArme=1 OR
+    idTypeArme=2) AND
+    (degat BETWEEN
+    25 AND
+    40)
+	
+SELECT
+	*
+FROM
+	personnage
+WHERE
+	level !=8 OR
+    level>7 OR
+    level<6
+	
+SELECT
+	COUNT(*) AS "Nb Perso <10"
+FROM
+	personnage
+WHERE
+	level<10
+	
+SELECT
+	AVG(degat)
+FROM
+	arme
+WHERE
+	levelMin BETWEEN
+    3 AND
+    7
+	
+
